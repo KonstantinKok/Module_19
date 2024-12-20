46f2fa4b1120cac7586901c4d314d88e19d118f6
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-12-20 12:02:47 +0300

    QuerySet
    Домашнее задание по теме "QuerySet запросы в базу данных"

--- module19/task1/models.py
+++ module19/task1/models.py
@@ -10,12 +10,12 @@ class Buyer(models.Model):
         return self.name
 
 class Game(models.Model):
-    title = models.CharField(max_length=255)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
     size = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyers = models.ManyToManyField(Buyer, related_name='games')
+    buyer = models.ManyToManyField(Buyer, related_name='games')
 
     def __str__(self):
         return self.title
